4a2f50f8374f9defd4d9a270acba50d05c0e36ce
vishnuw-ux <vishnuamrita18@gmail.com>
2026-05-02 12:30:10 +0530

    reverse_linked_list.c

---
+++ arrays/arrays/arrays/arrays/strings/strings/searching/sorting/arrays/sorting/linked_lists/reverse_linked_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* reverse(struct Node* head) {
+    struct Node* prev = NULL;
+    struct Node* curr = head;
+    struct Node* next = NULL;
+
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+}
